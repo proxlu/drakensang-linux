be4eb39729a6c1096c3d35ac5a37cd06ee8ae47c
Eduardo Solano <105125779+proxlu@users.noreply.github.com>
2023-09-17 16:54:35 -0300

    Add files via upload

---
+++ drakensang.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+unless File.exist?("#{ENV['HOME']}/.wine/drive_c/Program Files (x86)/Drakensang Online/thinclient.exe")
+  system('wget -P /tmp https://github.com/proxlu/drakensang-linux/raw/main/dro_setup.exe')
+  system('wine /tmp/dro_setup.exe')
+end
+
+unless File.exist?("#{ENV['HOME']}/.wine/drive_c/Program Files (x86)/Drakensang Online/extramaus")
+  system('wget -P /tmp https://github.com/proxlu/drakensang-linux/raw/main/extramaus.c')
+  system('gcc /tmp/extramaus.c -o ~/.wine/drive_c/Program\ Files\ \(x86\)/Drakensang\ Online/extramaus -lX11 -lXext -g')
+end
+
+system('~/.wine/drive_c/Program\ Files\ \(x86\)/Drakensang\ Online/extramaus&wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Drakensang\ Online/thinclient.exe&&kill $!')
